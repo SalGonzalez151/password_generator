d93a37fa4f33b7edc091ae31c29d91c9a4c1bf2a
Sal Gonzalez <imdone151@gmail.com>
2023-09-01 12:04:40 -0700

    added parts of the function and more variables to the code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordlist = [];
+var promptChoice = true;
 
 // Array of special characters to be included in password
 var specialCharacters = [
@@ -7,10 +9,12 @@ var specialCharacters = [
 ];
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
+
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -27,3 +31,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  passwordlist = [];
+  var passwordLength = prompt(" Please enter lenght of password between 8 and 126 characters")
+  if (passwordLength < 8 || passwordLength > 126 || isNaN(passwordLength)) {
+    alert("Please enter a number between 8 and 126 characters");
+    return false
+  }
+
+  var useLowercase = confirm('Do you want to use lowercase letters?');
+  var useUppercase = confirm('Do you want to use uppercase letters?');
+  var useSpecialCharacters = confirm('Do you want to use special characters?');
+  var useNumericCharacters = confirm('Do you want to use numeric characters?');
+
+
+  if (useLowercase === false && 
+      useUppercase === false &&
+      useSpecialCharacters === false &&
+      useNumericCharacters === false ) {
+        alert('Please select one option for a password');
+      }
+
+
+
+}
