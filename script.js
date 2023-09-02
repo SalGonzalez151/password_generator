d3732c4512ace2ec215013bbc52efa90cc8f0222
Sal Gonzalez <imdone151@gmail.com>
2023-09-01 19:10:53 -0700

    Added: if statements to add the letters to the password

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function generatePassword() {
   var passwordLength = prompt(" Please enter lenght of password between 8 and 126 characters")
   if (passwordLength < 8 || passwordLength > 126 || isNaN(passwordLength)) {
     alert("Please enter a number between 8 and 126 characters");
-    return false
+    return false;
   }
 
   var useLowercase = confirm('Do you want to use lowercase letters?');
@@ -46,13 +46,25 @@ function generatePassword() {
   var useNumericCharacters = confirm('Do you want to use numeric characters?');
 
 
-  if (useLowercase === false && 
-      useUppercase === false &&
-      useSpecialCharacters === false &&
-      useNumericCharacters === false ) {
-        alert('Please select one option for a password');
-      }
+  if (useLowercase === false &&
+    useUppercase === false &&
+    useSpecialCharacters === false &&
+    useNumericCharacters === false) {
+    alert('Please select one option for a password');
+  }
 
-
+  var charactersAdded = [];
+  if (useLowercase === true) {
+    charactersAdded = charactersAdded.concat(useLowercase);
+  }
+  if (useUppercase === true) {
+    charactersAdded = charactersAdded.concat(useUppercase);
+  }
+  if (useSpecialCharacters === true) {
+    charactersAdded = charactersAdded.concat(useSpecialCharacters);
+  }
+  if (useNumericCharacters === true) {
+    charactersAdded = charactersAdded.concat(useNumericCharacters);
+  }
 
 }
